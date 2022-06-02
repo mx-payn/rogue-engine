0da1f758556645f8e1bca5f75d8d66bdefe2ad0e
Max Franz <maex.payn@googlemail.com>
2022-06-02 17:35:33 +0200

    added Logger test cases

--- test/source/core/utils/Logger.cpp
+++ test/source/core/utils/Logger.cpp
@@ -5,8 +5,21 @@
 
 TEST_CASE("Logger") {
   CHECK(Rogue::Logger::Init(Rogue::LogLevel::Trace) == true);
+  CHECK(Rogue::Logger::GetLogger().get() != nullptr);
   CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::trace);
 
+  Rogue::Logger::SetLogLevel(Rogue::LogLevel::None);
+  CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::off);
+  Rogue::Logger::SetLogLevel(Rogue::LogLevel::Fatal);
+  CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::critical);
+  Rogue::Logger::SetLogLevel(Rogue::LogLevel::Error);
+  CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::err);
+  Rogue::Logger::SetLogLevel(Rogue::LogLevel::Warn);
+  CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::warn);
   Rogue::Logger::SetLogLevel(Rogue::LogLevel::Info);
   CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::info);
+  Rogue::Logger::SetLogLevel(Rogue::LogLevel::Debug);
+  CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::debug);
+  Rogue::Logger::SetLogLevel(Rogue::LogLevel::Trace);
+  CHECK(Rogue::Logger::GetLogger()->level() == spdlog::level::level_enum::trace);
 }
