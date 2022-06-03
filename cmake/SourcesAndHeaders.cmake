fe0689ad28607d5fd4fb17a4cd471872702b7040
Max Franz <maex.payn@googlemail.com>
2022-06-03 21:38:42 +0200

    clang formatting

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,12 +1,5 @@
-set(
-  rogue-headers
-  include/rogue/core/Game.hpp
-  include/rogue/core/utils/Logger.hpp
+set(rogue-headers include/rogue/core/Game.hpp include/rogue/core/utils/Logger.hpp
+                  include/rogue/core/utils/Assertion.hpp include/rogue/core/utils/DebugBreak.hpp
 )
 
-set(
-  rogue-sources
-  source/core/Game.cpp
-  source/core/utils/Logger.cpp
-  include/rogue/EntryPoint.cpp
-)
+set(rogue-sources source/core/Game.cpp source/core/utils/Logger.cpp include/rogue/EntryPoint.cpp)
