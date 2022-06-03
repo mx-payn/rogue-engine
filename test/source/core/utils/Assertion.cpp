c00bdbecd989a618c6e7b17b66cc73f54b1035c4
Max Franz <maex.payn@googlemail.com>
2022-06-03 21:36:41 +0200

    added Testcase for more Assertion coverage

--- test/source/core/utils/Assertion.cpp
+++ test/source/core/utils/Assertion.cpp
@@ -4,5 +4,6 @@
 
 TEST_CASE("Assertion") {
   CHECK(Rogue::Assertion::AssertFn(true, 0, ""));
+  CHECK(Rogue::Assertion::AssertFn(true, 0, "Test assertion with {} Text.", "format"));
   CHECK(!Rogue::Assertion::AssertFn(false, 0, ""));
 }
