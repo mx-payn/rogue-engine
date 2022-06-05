4805200f5957c2a585b18b3632c8207c1006204a
Max Franz <maex.payn@googlemail.com>
2022-06-05 19:26:05 +0200

    smart pointer wrappers

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,5 +1,7 @@
-set(rogue-headers include/rogue/core/Game.hpp include/rogue/core/utils/Logger.hpp
-                  include/rogue/core/utils/Assertion.hpp include/rogue/core/utils/DebugBreak.hpp
+set(
+  rogue-headers include/rogue/core/Game.hpp include/rogue/core/utils/Logger.hpp
+  include/rogue/core/utils/Assertion.hpp include/rogue/core/utils/DebugBreak.hpp
+  include/rogue/core/memory/SmartPointer.hpp
 )
 
 set(rogue-sources source/core/Game.cpp source/core/utils/Logger.cpp include/rogue/EntryPoint.cpp)
