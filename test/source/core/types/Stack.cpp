eb49a611c19fdaa65c91118f1cb1157d698b0f3b
Max Franz <maex.payn@googlemail.com>
2022-06-10 18:50:38 +0200

    test comments

--- test/source/core/types/Stack.cpp
+++ test/source/core/types/Stack.cpp
@@ -3,12 +3,14 @@
 #include <doctest/doctest.h>
 
 TEST_CASE("Stack - Push Peek Pop") {
+  // initialization
   Rogue::Stack<int> stack(10);
   CHECK(stack.GetCount() == 0);
   CHECK(stack.GetSize() == 10);
   CHECK(stack.IsEmpty() == true);
   CHECK(stack.IsFull() == false);
 
+  // Push elements, all but last
   for (int i = 0; i < 9; i++) {
     stack.Push(i);
     CHECK(stack.GetCount() == i + 1);
@@ -16,11 +18,13 @@ TEST_CASE("Stack - Push Peek Pop") {
     CHECK(stack.IsFull() == false);
   }
 
+  // Push last element
   stack.Push(9);
   CHECK(stack.GetCount() == 10);
   CHECK(stack.IsEmpty() == false);
   CHECK(stack.IsFull() == true);
 
+  // Peek and Pop all but last elements
   for (int i = 10; i > 1; i--) {
     CHECK(stack.Peek() == i - 1);
     int value = stack.Pop();
@@ -30,6 +34,7 @@ TEST_CASE("Stack - Push Peek Pop") {
     CHECK(stack.IsFull() == false);
   }
 
+  // Peek and Pop last element
   CHECK(stack.Peek() == 0);
   int value = stack.Pop();
   CHECK(value == 0);
