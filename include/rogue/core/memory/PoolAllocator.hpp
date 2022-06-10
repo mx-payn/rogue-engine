756996ba921079db3759db2fb2a84b1d0c665c77
Max Franz <maex.payn@googlemail.com>
2022-06-10 18:35:30 +0200

    reformat to native Stack implementation

--- include/rogue/core/memory/PoolAllocator.hpp
+++ include/rogue/core/memory/PoolAllocator.hpp
@@ -2,10 +2,8 @@
 #define POOLALLOCATOR_H_
 
 #include <cstddef>
-#include <memory>
-#include <stack>
-#include <utility>
 
+#include "rogue/core/types/Stack.hpp"
 #include "rogue/core/utils/Assertion.hpp"
 
 #define ROGUE_POOL_ALLOCATOR_DEFAULT_SIZE 1024
@@ -14,14 +12,14 @@ namespace Rogue {
 
   template <typename T> class PoolAllocator {
   public:
-    explicit PoolAllocator(size_t size = ROGUE_POOL_ALLOCATOR_DEFAULT_SIZE) : m_Size(size) {
+    explicit PoolAllocator(size_t size = ROGUE_POOL_ALLOCATOR_DEFAULT_SIZE)
+        : m_Size(size), m_FreeChunks(size) {
       // allocate amount of objects / chunks
-      // m_First = new T[size];
       m_First = (T*)malloc(sizeof(T) * size);
       m_Last = m_First + (size - 1);
 
       for (int i = m_Size - 1; i >= 0; i--) {
-        m_FreeChunks.push(m_First + i);
+        m_FreeChunks.Push(m_First + i);
       }
     }
 
@@ -37,14 +35,12 @@ namespace Rogue {
     }
 
     template <typename... Args> T* allocate(Args&&... args) {
-      if (m_FreeChunks.empty()) return nullptr;  // no more space to allocate
+      if (m_FreeChunks.IsEmpty()) return nullptr;  // no more space to allocate
 
-      T* poolChunk = m_FreeChunks.top();
+      T* poolChunk = m_FreeChunks.Pop();
 
       // placment new of object
       T* returnValue = new (poolChunk) T(std::forward<Args>(args)...);
-
-      m_FreeChunks.pop();
 
       return returnValue;
     }
@@ -57,11 +53,11 @@ namespace Rogue {
 
       // object dextructor call and resetting head
       data->~T();
-      m_FreeChunks.push(data);
+      m_FreeChunks.Push(data);
     }
 
-    uint32_t GetObjectCount() const { return m_Size - m_FreeChunks.size(); }
-    size_t GetFreeChunkCount() const { return m_FreeChunks.size(); }
+    uint32_t GetObjectCount() const { return m_Size - m_FreeChunks.GetCount(); }
+    size_t GetFreeChunkCount() const { return m_FreeChunks.GetCount(); }
     size_t GetSize() const { return m_Size; }
 
   private:
@@ -69,7 +65,7 @@ namespace Rogue {
     T* m_First = nullptr;
     T* m_Last = nullptr;
 
-    std::stack<T*> m_FreeChunks;
+    Stack<T*> m_FreeChunks;
   };
 
 }  // namespace Rogue
